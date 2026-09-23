18080c83b1c794cd360504463845061a3a35f845
grimdcc74-netizen <grimdcc74@gmail.com>
2026-09-23 23:09:15 +0200

    Split frontend vendor bundles

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "supabase",
+              test: /node_modules[\\/]@supabase[\\/]/,
+              priority: 2,
+            },
+            {
+              name: "react",
+              test: /node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+              priority: 2,
+            },
+            {
+              name: "vendor",
+              test: /node_modules[\\/]/,
+              priority: 1,
+            },
+          ],
+        },
+      },
+    },
+  },
+});
